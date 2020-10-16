90dc737c4b6325c2e5785ad1c248a831acabc8ea
allai-kef <alice@kefrobotics.com>
2020-10-15 23:20:10 -0400

    detail transfer works

--- filter.py
+++ filter.py
@@ -14,38 +14,18 @@ class BilateralFilter():
         self.apath = apath
         self.fpath = fpath
         self.imga = (io.imread(apath)) / 255.0
-        self.flat_dim = self.imga.shape[0] * self.imga.shape[1]
+        self.imgf = (io.imread(fpath)) / 255.0
 
-        self.img_rc = self.imga[:,:,0]
-        self.img_gc = self.imga[:,:,1]
-        self.img_bc = self.imga[:,:,2]
-
-        # range 0 ... 255
-        self.rmin = np.min(self.img_rc)
-        self.gmin = np.min(self.img_gc)
-        self.bmin = np.min(self.img_bc)
-
-        self.rmax = np.max(self.img_rc)
-        self.gmax = np.max(self.img_gc)
-        self.bmax = np.max(self.img_bc)
-
-        self.sigs  = 25
-        # self.sigs  = 3.0
-        self.sigr  = 0.05
-
-        self.r_segs = int((self.rmax - self.rmin) / self.sigr)
-        self.g_segs = int((self.gmax - self.gmin) / self.sigr)
-        self.b_segs = int((self.bmax - self.bmin) / self.sigr)
         self.prev_weights = np.zeros((self.imga.shape[0], self.imga.shape[1]))
 
     def gr(self, mu, sigma):
         return np.exp(-np.square(mu) / (2.0 * np.square(sigma)))
 
-    def get_weights(self, imgc, ij):
-        ij0 = ij - self.sigr
-        ij2 = ij + self.sigr
-        wj  = (ij2  - imgc) / self.sigr
-        wj0 = (imgc - ij0) / self.sigr
+    def get_weights(self, imgc, ij, sigr):
+        ij0 = ij - sigr
+        ij2 = ij + sigr
+        wj  = (ij2  - imgc) / sigr
+        wj0 = (imgc - ij0) / sigr
 
         weights_j   = np.where((ij <= imgc) & (imgc < ij2), wj, 0)
         weights_j0  = np.where((ij0 <= imgc) & (imgc < ij), wj0, 0)
@@ -57,61 +37,115 @@ class BilateralFilter():
 
         return weights
 
-    def filter_channel(self, channel):
+    def filter_channel(self, channel, joint, flash, sigs, sigr):
         print("filter channel")
-        imgc = self.img_rc
-        segs = self.r_segs
-        cmax = self.rmax
-        cmin = self.rmin
+        if (flash): imgc = self.imgf[:,:,channel]
+        else: imgc = self.imga[:,:,channel]
 
-        if(channel == 1):
-            imgc = self.img_gc
-            segs = self.g_segs
-            cmax = self.gmax
-            cmin = self.gmin
-
-        if(channel == 2):
-            imgc = self.img_bc
-            segs = self.b_segs
-            cmax = self.bmax
-            cmin = self.bmin
+        imgcf = self.imgf[:,:,channel]
+        cmax = np.max(imgc)
+        cmin = np.min(imgc)
+        segs = int((cmax - cmin) / sigr)
 
         J = np.zeros((imgc.shape[0], imgc.shape[1]))
-        kernel_dim = int(3 * self.sigs)
+        kernel_dim = int(3 * sigs)
 
         for j in range(segs + 1):
-            # print(str(j) + "/" + str(segs))
             ij = cmin + j * (cmax - cmin)/segs
-            Gj = self.gr(imgc - ij, self.sigr)
-            Kj = cv2.GaussianBlur(Gj, (kernel_dim, kernel_dim), self.sigs)
+            Gj = self.gr(imgc - ij, sigr)
+            if (joint):
+                Gj = self.gr(imgcf - ij, sigr)
+            else:
+                Gj = self.gr(imgc - ij, sigr)
+
+            Kj = cv2.GaussianBlur(Gj, (kernel_dim, kernel_dim), sigs)
             Hj = np.multiply(Gj, imgc)
-            # print(np.subtract(Kj, Gj))
-            Hsj = cv2.GaussianBlur(Hj, (kernel_dim, kernel_dim), self.sigs)
+            Hsj = cv2.GaussianBlur(Hj, (kernel_dim, kernel_dim), sigs)
             Jj = np.divide(Hsj, Kj)
 
-            min_value = np.nanmin(Jj)
             nan_ids = np.argwhere(np.isnan(Jj))
             for nid in nan_ids:
                 Jj[nid] = 0
 
-            J = np.add(J, np.multiply(Jj, self.get_weights(imgc, ij)))
+            if (joint):
+                weights = self.get_weights(imgcf, ij, sigr)
+            else:
+                weights = self.get_weights(imgc, ij, sigr)
+
+            J = np.add(J, np.multiply(Jj, weights))
 
         return J
 
-    def filter(self):
-        frc = self.filter_channel(0)
-        fgc = self.filter_channel(1)
-        fbc = self.filter_channel(2)
+    def bilateral_filter(self, flash, sigs, sigr):
+        filter_r = self.filter_channel(0, 0, flash, sigs, sigr)
+        filter_g = self.filter_channel(1, 0, flash, sigs, sigr)
+        filter_b = self.filter_channel(2, 0, flash, sigs, sigr)
 
-        print(np.max(frc), np.max(fgc), np.max(fbc))
-        img_bf = np.dstack((frc, fgc, fbc))
-        fig = plt.figure()
-        fig.add_subplot(2,1,1)
-        plt.imshow(np.clip(img_bf, 0, 1), cmap='gray')
-        fig.add_subplot(2,1,2)
-        plt.imshow(self.imga)
+        img_filter = np.dstack((filter_r, filter_g, filter_b))
+
+        plt.imshow(img_filter)
         plt.show()
-        return img_bf
+
+        return img_filter
+
+    def joint_filter(self, sigs, sigr):
+        joint_r = self.filter_channel(0, 1, 0, sigs, sigr)
+        joint_g = self.filter_channel(1, 1, 0, sigs, sigr)
+        joint_b = self.filter_channel(2, 1, 0, sigs, sigr)
+
+        img_joint = np.dstack((joint_r, joint_g, joint_b))
+
+        plt.imshow(img_joint)
+        plt.show()
+
+        return img_joint
+
+    def detail_transfer(self):
+        eps = 0.0001
+        # Fbase = self.bilateral_filter(1, 25, 0.05)
+        # Anr = self.joint_filter(25, 0.05)
+        Fbase = np.clip(io.imread("flash_bilateral_filter.png"), 0, 255) / 255.0
+        Anr = np.clip(io.imread("joint_filter.png"), 0, 255) / 255.0
+
+        detail = np.divide((self.imgf + eps), (Fbase + eps))
+        plt.imshow(self.imgf + eps)
+        plt.show()
+
+        nan_ids = np.argwhere(np.isnan(detail))
+        print(detail)
+        min_value = np.min(detail)
+        for nid in nan_ids:
+            detail[nid] = min_value;
+
+        detail = np.clip(detail, 0, 1)
+
+        plt.imshow(detail)
+        plt.show()
+        Adet = np.multiply(Anr, detail)
+        Adet = np.clip(Adet, 0, 1)
+
+
+        # fig = plt.figure()
+        # fig.add_subplot(2,1,1)
+        # plt.imshow(Anr)
+        # fig.add_subplot(2,1,2)
+        plt.imshow(Adet)
+        plt.show()
+        io.imsave("detail_transfer.png", Adet)
+        return Adet
+
+    def get_mask(self, thresh):
+        Alin_p = io.imread("")
+        Flin = io.imread("")
+        Alin = Alin_p * (() / ())
+
+        mask = np.where(Flin - Alin <= thresh, 1, 0)
+        return mask
+
+    def apply_mask(self):
+        Adet = self.detail_transfer()
+        mask = self.get_mask(0.05)
+        Afinal = (1.0 - mask)*Adet + mask*self.imga
 
 # class JointBilateralFilter():
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from filter import BilateralFilter
-
+from skimage import io
 apath = "data/lamp/lamp_ambient.tif"
 fpath = "data/lamp/lamp_flash.tif"
 sigs  = 10.0
@@ -7,7 +7,11 @@ sigr  = 10.0
 
 def main():
     bf = BilateralFilter(apath, fpath, sigs, sigr)
-    bf.filter()
+    # Fbase = bf.bilateral_filter(1, 25, 0.05)
+    # io.imsave("flash_bilateral_filter.png", Fbase)
+    # Anr = bf.joint_filter(5, 0.05)
+    # io.imsave("joint_filter.png", Anr)
+    bf.detail_transfer()
 
 
 if __name__ == "__main__":
